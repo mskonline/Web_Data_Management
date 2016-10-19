36fa428b390cc2821b475e761946e0827428d9a4
Sai Kumar Manakan <msk.mymails@gmail.com>
2016-10-18 21:33:13 -0500

    Computed visibility Added units selection

--- Project_1/weather.js
+++ Project_1/weather.js
@@ -1,46 +1,76 @@
 /*
-Student Name:  Sai Kumar Mankan
-ID: 1001236131
-Email: saikumar.manakan@mavs.uta.edu
+  Student Name:  Manakan, Sai Kumar
+  ID: 1001236131
+  Email: saikumar.manakan@mavs.uta.edu
 */
 
 var api_key = "fe68ebb8ef6e83fa4d3208acab2b2d68";
 
+function initPage(){
+  var input = document.getElementById('form-input');
+
+  // Adding KeyPress event for the 'Enter' key
+  input.addEventListener('keypress', function(e){
+    var keycode = e.keyCode;
+
+    // 'Enter' key is pressed
+    if(keycode == 13)
+      sendRequest();
+  });
+}
+
 function sendRequest () {
-    var elem = document.getElementById('output');
-    elem.style.display = "none";
+  var elem = document.getElementById('output');
+  elem.style.display = "none";
 
-    var xhr = new XMLHttpRequest();
-    var city = encodeURI(document.getElementById("form-input").value);
+  var xhr = new XMLHttpRequest();
+  var city = encodeURI(document.getElementById("form-input").value);
 
-    if(city === '') {
-      alert('Please enter city.');
-      return;
-    }
+  var units;
+  if(document.getElementById('def_units').checked)
+    units = 'default';
+  else if (document.getElementById('met_units').checked)
+    units = 'metric';
+  else {
+    units = 'imperial';
+  }
 
-    xhr.open("GET", "proxy.php?q="+city+"&appid="+api_key+"&format=json&units=metric", true);
-    xhr.setRequestHeader("Accept","application/json");
+  if(city === '') {
+    alert('Please enter city.');
+    return;
+  }
 
-    xhr.onreadystatechange = function () {
-        if (this.readyState == 4) {
-            var json = JSON.parse(this.responseText);
-            displayResults(json);
-        }
-    };
-    xhr.send(null);
+  xhr.open("GET", "proxy.php?q=" + city + "&appid=" + api_key + "&format=json&units=" + units, true);
+  xhr.setRequestHeader("Accept","application/json");
+
+  xhr.onreadystatechange = function () {
+      if (this.readyState == 4) {
+          var json = JSON.parse(this.responseText);
+          displayResults(json);
+      }
+  };
+  xhr.send(null);
 }
 
 function displayResults(resultJson){
-
   if(resultJson == undefined){
     alert('Something went wrong. Please try again');
     return;
   }
 
+  var units;
+  if(document.getElementById('def_units').checked)
+    units = ' Kelvin';
+  else if (document.getElementById('met_units').checked)
+    units = ' Celcius';
+  else {
+    units = ' Fahrenheit ';
+  }
+
   var body = document.body;
   var elem = document.getElementById('wIcon');
-  var url = 'http://openweathermap.org/img/w/' + resultJson.weather[0].icon + '.png';
-  wIcon.style.backgroundImage = 'url('+ url + ')';
+  var iconUrl = 'http://openweathermap.org/img/w/' + resultJson.weather[0].icon + '.png';
+  wIcon.style.backgroundImage = 'url('+ iconUrl + ')';
 
   elem = document.getElementById('cityName');
   elem.innerHTML = resultJson.name;
@@ -61,18 +91,23 @@ function displayResults(resultJson){
   elem.innerHTML = resultJson.main.humidity;
 
   elem = document.getElementById('temperature');
-  elem.innerHTML = resultJson.main.temp + ' Celcius';
+  elem.innerHTML = resultJson.main.temp + units;
 
   elem = document.getElementById('minTemperature');
-  elem.innerHTML = resultJson.main.temp_min + ' Celcius';
+  elem.innerHTML = resultJson.main.temp_min + units;
 
   elem = document.getElementById('maxTemperature');
-  elem.innerHTML = resultJson.main.temp_max + ' Celcius';
+  elem.innerHTML = resultJson.main.temp_max + units;
 
   elem = document.getElementById('clouds');
   elem.innerHTML = resultJson.clouds.all;
 
+  elem = document.getElementById('visibility');
+  elem.innerHTML = findVisibility(resultJson.weather[0].id);
+
   elem = document.getElementById('advise');
+
+  var advise;
 
   // Check the weather code.
   if(willItRain(resultJson.weather[0].id)) {
@@ -82,7 +117,7 @@ function displayResults(resultJson){
     advise = "It will be cold today. Wear a coat";
   } // Clear day
   else {
-    var advise = "It's a clear day today";
+    advise = "It's a clear day today";
   }
 
   elem.innerHTML = advise;
@@ -104,7 +139,23 @@ function willItRain(weatherCode){
 
   if(code >= 2 && code <=5)
     return true;
-  else {
+  else
     return false;
-  }
+}
+
+function findVisibility(weatherCode){
+  var code = String(weatherCode).charAt(0);
+  /*
+    From OpenWeather API doc.
+    https://openweathermap.org/weather-conditions
+    2xx - Clear
+    9xx - Extreme (as in Hotness, Coldness etc)
+
+    For only 2xx, 9xx we will assume Good visibility else Bad
+    (All other codes eg Rain, Thunderstorm, Snow will have bad visibility)
+  */
+  if(code == 8 || code == 9)
+    return 'Good';
+  else
+    return 'Bad';
 }
