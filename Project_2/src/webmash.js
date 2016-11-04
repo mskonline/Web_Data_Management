17576c084748f5a29c60252afd92085a972835e3
Sai Kumar Manakan <msk.mymails@gmail.com>
2016-11-03 23:22:40 -0500

    Directory changes

--- Project_2/src/webmash.js
+++ Project_2/src/webmash.js
@@ -2,13 +2,17 @@
   Student Name:  Manakan, Sai Kumar
   ID: 1001236131
   Email: saikumar.manakan@mavs.uta.edu
+	Project Name: Web Mash
+  Due date: Oct 26 2016
 */
 
+// Geonames username
 var username = "saikumarm";
+
 var request = new XMLHttpRequest();
-var gmarker;
 
 var map,
+		gmarker,
 		geocoder,
 		glocation,
 		gAddress,
@@ -16,6 +20,7 @@ var map,
 		textArea,
 		clearBtn;
 
+// Initializing variables after document load
 window.onload = function(){
 	textArea = document.getElementById('textArea');
 	clearBtn = document.getElementById('clearBtn');
@@ -32,13 +37,11 @@ function initMap() {
 	geocoder = new google.maps.Geocoder;
 	infowindow = new google.maps.InfoWindow;
 
-	//initialize map
+	//initializing the map
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: initCords,
 		zoom: 17
 	});
-
-	//reversegeocode();
 
 	//Initialize a mouse click event on map which then calls reversegeocode function
 	google.maps.event.addListener(map, 'click', function(event) {
@@ -60,6 +63,7 @@ function reversegeocode(location) {
 			map: map
 	});
 
+	// Saving the location
 	glocation = location;
 
   //get the latitude and longitude from the mouse click and get the address.
@@ -67,9 +71,11 @@ function reversegeocode(location) {
 	geocoder.geocode({'location': location}, function(results, status) {
     if (status === 'OK') {
       if (results[1]) {
+				// Saving the address
 				gAddress = results[1].formatted_address;
 
-				sendRequest(glocation)
+				// Calling geonames with the location
+				sendRequest(glocation);
       } else {
         window.alert('No results found');
       }
@@ -77,11 +83,12 @@ function reversegeocode(location) {
       window.alert('Geocoder failed due to: ' + status);
     }
   });
-}// end of geocodeLatLng()
+} // end of geocodeLatLng()
 
 function displayResult () {
     if (request.readyState == 4) {
 				try {
+					// Extracting details
 					var xml = request.responseXML.documentElement;
 					var temperature = xml.getElementsByTagName("temperature");
 					var windSpeed = xml.getElementsByTagName("windSpeed");
@@ -90,11 +97,14 @@ function displayResult () {
 					var t = temperature[0].innerHTML;
 					var ws = windSpeed[0].innerHTML;
 					var clds = clouds[0].innerHTML;
+
+					// Formatted message for inforwindow
 					var msg = 'Address: ' + gAddress + '<br>Temperature: '+ t + ' C' + '<br>Windspeed: ' + ws + '<br>Clouds: ' + clds;
 
 					infowindow.setContent(msg);
 	        infowindow.open(map, gmarker);
 
+					// Formatted message for the text area
 					msg = 'Address: ' + gAddress + ' - \n\tTemperature: '+ t + ' C' + ' Windspeed: ' + ws + ' Clouds: ' + clds;
 					textArea.append(msg + '\n');
 				} catch (e) {
@@ -102,12 +112,12 @@ function displayResult () {
 					alert('Something went wrong. Please try again');
 				}
     }
-}
+} // end of displayResult()
 
 function sendRequest (location) {
 		var lat = location.lat(), lng = location.lng();
     request.onreadystatechange = displayResult;
-    request.open("GET"," http://api.geonames.org/findNearByWeatherXML?lat="+lat+"&lng="+lng+"&username="+username,true);
+    request.open("GET","http://api.geonames.org/findNearByWeatherXML?lat=" + lat + "&lng=" + lng + "&username=" + username, true);
     //request.withCredentials = "true";
     request.send(null);
-}
+} // end of sendRequest()
